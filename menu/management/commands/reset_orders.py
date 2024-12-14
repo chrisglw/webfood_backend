3373463633a8d1153a1d1ffd74ff9aaaf95133a6
christian <lan21011@byui.edu>
2024-12-13 18:59:16 -0700

    add reset orders

---
+++ menu/management/commands/reset_orders.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from menu.models import Order, OrderItem
+from django.db import connection
+
+class Command(BaseCommand):
+    help = 'Delete all orders and reset primary key sequence'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Deleting all orders and order items...')
+        OrderItem.objects.all().delete()
+        Order.objects.all().delete()
+
+        self.stdout.write('Resetting primary key sequence...')
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM sqlite_sequence WHERE name='menu_order';")
+            cursor.execute("DELETE FROM sqlite_sequence WHERE name='menu_orderitem';")
+
+        self.stdout.write(self.style.SUCCESS('Successfully reset orders and order items!'))
